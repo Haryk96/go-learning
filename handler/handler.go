77a0ddc81a02ccbf2cd44a75cd73f6dd1880ee5b
Jan Harenčák <jan.harencak@firma.seznam.cz>
2023-03-13 19:57:18 +0100

    remove unused

--- handler/handler.go
+++ handler/handler.go
@@ -18,14 +18,11 @@ func JWTHandler(c *gin.Context) {
 	name := c.Query("name")
 	age := c.Query("age")
 
-	// Read request body return 400
-	// if it does not comply to my model
 	var requestBody models.UserCredentials
 	if err := c.BindJSON(&requestBody); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	cl.Println("Test cl")
 	cl.Printf("%s, %s\n", name, age)
 	cl.Printf("%+v\n", requestBody)
 	c.JSON(http.StatusCreated, requestBody)
